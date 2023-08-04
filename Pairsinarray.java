90d7c12ba9546cb01b30a372dcd355769cc37823
indrajit1705 <indrajitchandra123@gmail.com>
2023-08-05 00:48:34 +0530

    pairsofarray

---
+++ Pairsinarray.java
@@ -0,0 +1,15 @@
+public class Pairsinarray {
+
+    public static void printpairs(int num[]){
+        for (int i = 0; i < num.length; i++) {
+            for(int j = i+1; j < num.length; j++){
+                System.out.print("("+num[i]+","+num[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int num[] = {2,4,6,8,10};
+        printpairs(num);
+    }
+}
