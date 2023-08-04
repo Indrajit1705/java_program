4c07764cbad0c67cb320c0d04a6f3555a5bb3307
indrajit1705 <indrajitchandra123@gmail.com>
2023-08-05 01:35:02 +0530

    totalpairsupdate

--- Printsubarray.java
+++ Printsubarray.java
@@ -1,17 +1,22 @@
 public class Printsubarray {
     public static void subarray(int num[]){
+        int tp = 0;
         for (int i = 0; i < num.length; i++) {              //start = i
             for (int j = i; j < num.length; j++) {         // end = j
                 for (int k = i; k <= j; k++) {               //print
                     System.out.print(num[k]+" ");           //subarray
+                     
                 }
                 System.out.println();
+                tp++;
             }
             System.out.println();
+           
         }
+        System.out.println("total pairs = "+tp);
     }
     public static void main(String[] args) {
-        int num[] = {2,4,6,8,10,12};
+        int num[] = {2,4,6,8,10};
         subarray(num);
 
     }
