8a5cc0b3f4488f7862b2e5ffe365cddc7b449c3e
indrajit1705 <indrajitchandra123@gmail.com>
2023-08-11 11:41:40 +0530

    palindromeinstring

---
+++ Stringpalindrome.java
@@ -0,0 +1,15 @@
+public class Stringpalindrome {
+    public static boolean ispalindrome(String str){
+        for (int i = 0; i < str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){             //not a palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(ispalindrome(str));
+    }
+}
