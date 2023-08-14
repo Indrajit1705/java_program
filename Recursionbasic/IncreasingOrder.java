c74e2a3a71daa7a40f3ea71884537c049d593df8
indrajit1705 <indrajitchandra123@gmail.com>
2023-08-14 19:07:42 +0530

    recursion

---
+++ Recursionbasic/IncreasingOrder.java
@@ -0,0 +1,17 @@
+package Recursionbasic;
+
+public class IncreasingOrder {
+    public static void printinc(int n){
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+        printinc(n-1);
+        System.out.print(n+" ");
+        
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printinc(n);
+    }
+}
