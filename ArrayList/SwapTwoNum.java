9055a3db83c9d70654f5423540a85353a4eed335
indrajit1705 <indrajitchandra123@gmail.com>
2023-08-18 20:08:45 +0530

    swapbetweentwonum

---
+++ ArrayList/SwapTwoNum.java
@@ -0,0 +1,24 @@
+package ArrayList;
+import java.util.*;
+public class SwapTwoNum {
+    public static void swap(ArrayList<Integer> list,int index1,int index2){
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));         //swapping function
+        list.set(index2 ,temp);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        System.out.println(list);
+        //swap between index1 to index3
+        int index1 = 1 ,index2 = 3;
+        swap(list,index1,index2);
+        System.out.println(list);
+        
+    }
+}
